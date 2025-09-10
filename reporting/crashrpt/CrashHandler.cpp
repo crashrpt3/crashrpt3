b6d18b8d82bd35a02688a85a5476c07cde623e96
siren186 <765495939@qq.com>
2025-09-10 16:49:56 +0800

    fix(CrashRpt.dll): UnSetThreadExceptionHandlers error

--- reporting/crashrpt/CrashHandler.cpp
+++ reporting/crashrpt/CrashHandler.cpp
@@ -1029,7 +1029,7 @@ int CCrashHandler::UnSetThreadExceptionHandlers()
         signal(SIGFPE, handlers->m_prevSigFPE);
 
     if(handlers->m_prevSigILL!=NULL)
-        signal(SIGINT, handlers->m_prevSigILL);
+        signal(SIGILL, handlers->m_prevSigILL);
 
     if(handlers->m_prevSigSEGV!=NULL)
         signal(SIGSEGV, handlers->m_prevSigSEGV);
@@ -1274,6 +1274,7 @@ int CCrashHandler::AddVideo(DWORD dwFlags, int nDuration, int nFrameInterval,
 int CCrashHandler::GenerateErrorReport(
         PCR_EXCEPTION_INFO pExceptionInfo)
 {
+    ::MessageBox(0, 0, 0, 0);
 	ReInitializer re_initializer( * this ) ;
 
 	crSetErrorMsg(_T("Unspecified error."));
