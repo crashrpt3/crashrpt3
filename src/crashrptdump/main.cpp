0ea2695564b9fcdaa42f21bf0754622e5b232c99
siren186 <765495939@qq.com>
2025-09-11 14:30:33 +0800

    add messagebox when app crashed

--- src/crashrptdump/main.cpp
+++ src/crashrptdump/main.cpp
@@ -1,6 +1,13 @@
 ﻿#include "stdafx.h"
 #include "crashrpt/crashrpt.h"
 
+void onMiniDumpCreated(void* param, const wchar_t* directory)
+{
+    CString msg;
+    msg.Format(L"Application crashed, dump created.\n%scrashdump.dmp", directory);
+    ::MessageBox(nullptr, msg, L"Tips", MB_OK);
+}
+
 int WINAPI WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hInstPrev, _In_ LPSTR cmdline, _In_ int cmdshow)
 {
     LPCWSTR szCmdLineW = ::GetCommandLineW();
@@ -10,5 +17,5 @@ int WINAPI WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hInstPrev, _In_ LPST
     {
         return 1;
     }
-    return crCreateMiniDump(argv[1]);
+    return crCreateMiniDump(argv[1], &onMiniDumpCreated);
 }
